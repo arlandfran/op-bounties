4b995c165450d76c07cf80bdbf55d07437f2f1ac
Arland Torres <arland.torres@outlook.com>
2022-09-01 23:48:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OP Bounties
+
+Full-stack app where you can view the bounties of characters from the One Piece universe
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>One Piece Bounties</title>
+        <meta
+          name="description"
+          content="View the bounties of One Piece characters"
+        />
+      </Head>
+
+      <h1>hello world</h1>
+    </>
+  );
+};
+
+export default Home;
